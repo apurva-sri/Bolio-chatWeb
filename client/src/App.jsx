79c6a1b001f64f1b3d57541c91533b55b7196fd4
Apurva Srivastava <apurvasrivastava2403@gmail.com>
2026-01-11 22:22:23 +0530

    login register page bug fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { AuthProvider } from "./context/AuthContext";
 
-const Chat = () => <h1>Chat Page (Protected)</h1>;
+const Chat = () => <h1 className="text-2xl p-4">Chat Page (Protected)</h1>;
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
+          {/* Default route */}
+          <Route path="/" element={<Navigate to="/login" />} />
+
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+
           <Route
             path="/chat"
             element={
